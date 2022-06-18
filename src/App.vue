c151a19a5264e39e64abaf2c3dccecdeb31e899d
ahmet.keskin <ahmet.keskin@axata.com.tr>
2022-06-18 21:14:39 +0300

    api ile haberleşme yapıldı.

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
 <template>
+<div class="container">
+
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+ <Users/> <!-- componenti ekledik html kısmına -->
+ 
+</div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Users from './components/Users'  // ilk olarak componenti import ettik
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Users  // adına göre ekledik
   }
 }
 </script>
---
+++ src/components/Users.vue
@@ -0,0 +1,59 @@
+<template>
+
+<div>
+    HelloWorld
+</div>
+
+</template>
+
+
+
+
+<script>
+
+import axios from 'axios'  // axios import ettik compt.
+
+export default {
+
+data(){
+    return{
+     
+        name:null
+    }
+},
+
+created: function() {
+    axios.get("https://localhost:44332/api/Main/1").then((res) => {
+        this.name =res.data.Data[0].sName;
+        console.log(this.name);
+    });
+
+
+
+        // const req = async () => {
+        // const response = await axios.get('https://localhost:44332/api/Main/1')
+        // console.log(response)
+        // }
+        // req() 
+
+
+},
+
+
+
+
+
+
+
+
+
+
+};
+</script>
+
+
+
+
+
+<style>
+</style>
