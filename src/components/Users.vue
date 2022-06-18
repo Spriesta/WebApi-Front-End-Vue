d979c52bdee51495189f5a66bb9b560b2dca6369
ahmet.keskin <ahmet.keskin@axata.com.tr>
2022-06-19 01:55:41 +0300

    Listeleme yapıldı

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -1,9 +1,39 @@
 <template>
+<div class="container">  
+
+
+<table class="table">
+
+  <thead class="thead-dark">
+    <tr>
+      <th scope="id">ID</th>
+      <th scope="sName">Ad</th>
+      <th scope="sAge">Yaş</th>
+      <th scope="sSchoolNum">Okul Numarası</th>
+      <th scope="sFrom">Doğum Yeri</th>
+      <th scope="sGender">Cinsiyet</th>
+    </tr>   
+  </thead>
+
+  <tbody v-for="user in Users" :key="user.id">
+    <tr>
+      <th scope="row">{{user.id}}</th>
+      <td>{{user.sName}}</td>
+      <td>{{user.sAge}}</td>
+      <td>{{user.sSchoolNum}}</td>
+      <td>{{user.sFrom}}</td>
+      <td>{{user.sGender}}</td>
+
+    </tr>
+    
+  </tbody>
+
+</table>
 
 
 
+</div>
 </template>
-
 
 
 
@@ -15,21 +45,16 @@ export default {
 
 data(){
     return{
-     
-        name:null
+        Users:null
     }
 },
 
-// created: function() {
-//     axios.get("https://localhost:44332/api/Main/1").then((res) => {
-//         this.name =res.data.Data[0].sName;
-//         console.log(this.name);
-//     });  
-// },
-
-
-
-
+created: function() {
+    axios.get("https://localhost:44332/api/Main").then((res) => {
+        this.Users = res.data.Data;
+       // console.log(this.name);
+    });  
+},
 
 
 
@@ -37,7 +62,11 @@ data(){
 
 
 };
+
 </script>
+
+
+
 
 
 
